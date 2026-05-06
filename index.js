aff65459de44b3d4cf0003809e9b8dc1e26df8ad
Udita Singh <udita1294@gmail.com>
2026-05-07 02:55:12 +0530

    data stored in pinecone

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 dotenv.config();
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
 import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
-import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
 import { Pinecone } from '@pinecone-database/pinecone';
+import { PineconeStore } from '@langchain/pinecone';
 
 
 // Function to load and index the PDF document
@@ -12,6 +13,8 @@ async function indexDocument() {
     const pdfLoader = new PDFLoader(PDF_PATH);
     const rawDocs = await pdfLoader.load();
     // console.log('Raw Documents:', rawDocs.length);
+    console.log("Document loaded");
+
 
 
     // Chunking the document
@@ -21,6 +24,9 @@ async function indexDocument() {
   });
     const chunkedDocs = await textSplitter.splitDocuments(rawDocs);
     // console.log('Chunked Documents:', chunkedDocs.length);
+    console.log("Chunking completed");
+
+
 
 
     /* Vector embedding model (chunks to vectors)
@@ -28,14 +34,29 @@ async function indexDocument() {
     */
     const embeddings = new GoogleGenerativeAIEmbeddings({
         apiKey: process.env.GEMINI_API_KEY,
-        model: 'text-embedding-004',
+        model: "text-embedding-004",
     });
+    console.log("Embedding model configured")
 
-    
+
+
     /* Database configuration and storing the vectors
     Intialize Pinecone Client*/
     const pinecone = new Pinecone();
     const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+    console.log("Pinecone configured")
+
+
+
+
+    // Store the chunked documents in Pinecone
+    await PineconeStore.fromDocuments(chunkedDocs, embeddings, {
+        pineconeIndex,
+        maxConcurrency: 5,
+    });
+    console.log("Data stored in Pinecone successfully");
+
+
 
 }
 
