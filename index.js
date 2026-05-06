585d2a8da76a1754601dc03aacd600ec0027d648
Udita Singh <udita1294@gmail.com>
2026-05-07 02:14:11 +0530

    chunking of the document is done

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
 
+
+// Function to load and index the PDF document
 async function indexDocument() {
     const PDF_PATH = './dsa.pdf';
     const pdfLoader = new PDFLoader(PDF_PATH);
     const rawDocs = await pdfLoader.load();
+    // console.log('Raw Documents:', rawDocs.length);
 
-    console.log(rawDocs.length);
+
+    // Chunking the document
+    const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  });
+    const chunkedDocs = await textSplitter.splitDocuments(rawDocs);
+    // console.log('Chunked Documents:', chunkedDocs.length);
+
 }
 
 indexDocument();
